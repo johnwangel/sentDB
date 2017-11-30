bebf907c934778863e4db4d4286a3c3eab7b3a57
John Atkins <johnatkins1999@yahoo.com>
2017-11-30 13:13:30 -1000

    updated imports

--- csv/import.sql
+++ csv/import.sql
@@ -77,3 +77,7 @@ DELIMITER ',' CSV HEADER;
 COPY sentences(words,coded,complexity_id,keywords,"createdAt","updatedAt")
 FROM '/Users/johnatkins/MyProjects/sentDB/csv/sentences.csv'
 DELIMITER ',' CSV HEADER;
+
+COPY sentences(words,coded,complexity_id,keywords,"createdAt","updatedAt")
+FROM '/Users/johnatkins/MyProjects/sentDB/csv/sentences.csv'
+DELIMITER ',' CSV HEADER;
--- csv/import_data.sql
+++
@@ -1,3 +0,0 @@
-COPY sentences(words,coded,complexity_id,keywords,"createdAt","updatedAt")
-FROM '/Users/johnatkins/MyProjects/sentDB/csv/sentences.csv'
-DELIMITER ',' CSV HEADER;
