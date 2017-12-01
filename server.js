1e6302b0b6e93f10e29a9adb3d28445f45f5a12a
John Atkins <johnatkins1999@yahoo.com>
2017-11-30 18:17:39 -1000

    access controls

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ require('./passport')();
 
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cookieParser());
 app.use(
   session({
